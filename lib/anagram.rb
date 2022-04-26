6064cee48bcedaad81efaae82ea60c1cba748d43
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-25 22:34:26 -0500

    commit tree c2f69c511fc4fb5eecca2a5a50b615f995762617

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match anagrams
+        anagrams.filter do |item|
+            item.chars.sort == @word.chars.sort
+        end
+    end
+
+end
